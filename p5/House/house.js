e4f1d9e67629a6cbc71229beb092e32b68c9eae7
Gage13 <dplak1@ocdsb.ca>
2017-04-04 16:38:48 -0400

    Update house.js

--- p5/House/house.js
+++ p5/House/house.js
@@ -4,9 +4,9 @@ Dragi Plakalovic 2017-03-10
 Written on KhanAcademy */
 
 function setup() {
-  createCanvas(800, 400); // Canvas size.
- }
- 
+  size(720, 400); // Canvas size.
+}
+
 function draw() {
   var posX = 100;
   var posY = 100;
@@ -18,20 +18,30 @@ function draw() {
 
   // Sun.
   fill(247, 255, 0);
-  ellipse(posX + 225, posY - 40, houseWidth - 10, houseWidth - 10);
+  ellipse(posX*3 + 60, posY*0.6, houseWidth - 10, houseWidth - 10);
 
   // Clouds.
   fill(255, 255, 255);
 
   /* Cloud One */
-  ellipse(posX, posY, houseWidth - 65, houseWidth - 65);
-  ellipse(posX - 30, posY, houseWidth - 50, houseWidth - 50);
-  ellipse(posX - 60, posY, houseWidth - 65, houseWidth - 65);
+  ellipse(posX, posY, houseWidth*0.35, houseWidth*0.35);
+  ellipse(posX*0.70, posY, houseWidth/2, houseWidth/2);
+  ellipse(posX*0.40, posY, houseWidth*0.35, houseWidth*0.35);
 
   /* Cloud Two. */
-  ellipse(posX + 60, posY - 60, houseWidth - 65, houseWidth - 65);
-  ellipse(posX + 30, posY - 60, houseWidth - 50, houseWidth - 50);
-  ellipse(posX, posY - 60, houseWidth - 65, houseWidth - 65);
+  ellipse(posX*1.60, posY*0.40, houseWidth*0.35, houseWidth*0.35);
+  ellipse(posX*1.30, posY*0.40, houseWidth/2, houseWidth/2);
+  ellipse(posX, posY*0.40, houseWidth*0.35, houseWidth*0.35);
+  
+  /* Cloud Three */
+  ellipse(posX*6.15, posY, houseWidth*0.35, houseWidth*0.35);
+  ellipse(posX*6.45, posY, houseWidth/2, houseWidth/2);
+  ellipse(posX*6.75, posY, houseWidth*0.35, houseWidth*0.35);
+
+  /* Cloud Four. */
+  ellipse(posX*6.10, posY*0.40, houseWidth*0.35, houseWidth*0.35);
+  ellipse(posX*5.80, posY*0.40, houseWidth/2, houseWidth/2);
+  ellipse(posX*5.50, posY*0.40, houseWidth*0.35, houseWidth*0.35);
 
   /* House number 1 */
   
@@ -39,36 +49,36 @@ function draw() {
   triangle(posX - (houseWidth*0.4), posY + (houseWidth*0.87), posX + (houseWidth*2.3), posY + (houseWidth*0.87), posX + (houseWidth*0.95), posY*0.83); // Roof.
   
   fill(255, 255, 255);
-  rect(posX*0.8, posY*0.87, houseWidth*2.3, houseWidth*1.62); // Base
+  rect(posX*0.8, posY*1.87, houseWidth*2.3, houseWidth*1.62); // Base
 
   fill(21, 122, 189);
-  rect(posX, posY + 106, houseWidth - 30, houseWidth - 50); // Left Window
+  rect(posX, posY*2 + 6, houseWidth*0.7, houseWidth/2); // Left Window
   rect(posX + 120, posY + 106, houseWidth - 30, houseWidth - 50); // Right Window
-
+  
   fill(102, 51, 0);
-  rect(posX + 69, posY + 167, houseWidth - 50, houseWidth - 18); // Door
+  rect(posX*1.69, posY*2 + 67, houseWidth/2, houseWidth*0.82); // Door
   
   /* House number 2 */
   
   fill(255, 255, 255);
-  rect(posX*4 + 20, posY + 87, houseWidth + 130, houseWidth + 62); // Base
+  rect(posX*4 + 20, posY*1.87, houseWidth*2 + 30, houseWidth*1.62); // Base
   
   /* Windows */
   fill(21, 122, 189);
-  rect(posX*4 + 40, posY + 106, houseWidth - 30, houseWidth - 50); // Left Window
-  rect(posX*4 + 160, posY + 106, houseWidth - 30, houseWidth - 50); // Right Window
+  rect(posX*4 + 40, posY*2 + 6, houseWidth*0.7, houseWidth/2); // Left Window
+  rect(posX*5 + 60, posY*2 + 6, houseWidth*0.7, houseWidth/2); // Right Window
   
   /* Roof */
   
   fill(255, 0, 0);
-  triangle(400, 187, 670, 187, 525, 87);
+  triangle(posX*4, posY*1.87, posX*6 + 70, posY*1.87, houseWidth*5 + (houseWidth/4), houseWidth*0.87);
   
   /* Door */
   
   fill(102, 51, 0);
-  rect(509, 267, 50, 82);
+  rect(posX*5 + 9, posY*2  + 67, houseWidth/2, houseWidth*0.82);
 
   // Ground
   fill(33, 252, 13);
-  rect(posX - posX, posY + 250, houseWidth + 700, houseWidth - 50);
+  rect(posX*0, posY*3 + 50, houseWidth*8, houseWidth/2);
 }
