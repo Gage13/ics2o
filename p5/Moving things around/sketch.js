50ebd948f377e3c1c8bdc49fc76efdbdea797f75
Gage13 <dplak1@ocdsb.ca>
2017-04-24 11:20:19 -0400

    Update sketch.js

--- p5/Moving things around/sketch.js
+++ p5/Moving things around/sketch.js
@@ -43,4 +43,14 @@ function draw() {
 	if (velocityX <= 0) {
 		velocityX = 0;
 	}
+	/* draw the car body
+	fill(255, 0, 115);
+	rect(x, 200, 100, 20);
+	rect(x + 15, 178, 70, 40);
+
+	// draw the wheels
+	fill(77, 66, 66);
+	ellipse(x + 25, 221, 24, 24);
+	ellipse(x + 75, 221, 24, 24); */
+
 }
