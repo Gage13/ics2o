c309139054f479618f697c32a5615691b6258771
Gage13 <dplak1@ocdsb.ca>
2017-04-24 12:17:09 -0400

    Add files via upload

--- p5/Moving things around/sketch.js
+++ p5/Moving things around/sketch.js
@@ -8,14 +8,33 @@ var posX = 240; // X position of gravity ball
 var posY = 10; // Initial position Y of ball
 var speedY = 3; // Initial speed.
 
-// Friction ball position and speed.
+// Friction car position and speed.
 var centerX = 100; // Initial position X
 var centerY = 850; // Y position that remains the same
-var velocityX = 47.5; // Initial speed
+var velocityX = 47; // Initial speed
 
 function setup() {
-	var myCanvas = createCanvas(1280, 900);
-	myCanvas.parent('MoveThings');
+	createCanvas(1280, 900);
+}
+
+// Function that draws friction car
+function car() {
+	// draw the car body
+	fill(255, 0, 115); // Car colour
+	rect(centerX - 100, centerY - 20, 100, 20); // Small car part
+	rect((centerX + 15) - 100, centerY - 40, 70, 40); // Large car part
+	// draw the wheels
+	fill(77, 66, 66); // Wheel colour
+	ellipse((centerX + 25) - 100, centerY, 24, 24); // Left wheel
+	ellipse((centerX + 75) - 100, centerY, 24, 24); // Right wheel
+	
+	centerX = centerX + velocityX; // Move the ball by adding velocity to it.
+	velocityX--; // To simulate friction, velocity of the ball drops by 1 pixel each time.
+	
+	// I do not want the car to go left and accelarate. I want to stop it when velocity drops to zero.
+	if (velocityX <= 0) {
+		velocityX = 0;
+	}
 }
 
 function draw() {
@@ -33,20 +52,6 @@ function draw() {
 		speedY = -speedY;
 	}
 	
-	// draw the car body
-	fill(255, 0, 115); // Car colour
-	rect(centerX - 100, centerY - 20, 100, 20); // Small car part
-	rect((centerX + 15) - 100, centerY - 40, 70, 40); // Large car part
-	// draw the wheels
-	fill(77, 66, 66); // Wheel colour
-	ellipse((centerX + 25) - 100, centerY, 24, 24); // Left wheel
-	ellipse((centerX + 75) - 100, centerY, 24, 24); // Right wheel
-	
-	centerX = centerX + velocityX; // Move the ball by adding velocity to it.
-	velocityX--; // To simulate friction, velocity of the ball drops by 1 pixel each time.
-	
-	// I do not want the ball to go left and accelarate. I want to stop it when velocity drops to zero.
-	if (velocityX <= 0) {
-		velocityX = 0;
-	}
+	/* Function calls for car. */
+	car();
 }
