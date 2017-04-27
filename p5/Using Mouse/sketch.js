86792822e328b68a9fb67d07b3883a81601029ef
Gage13 <dplak1@ocdsb.ca>
2017-04-27 13:57:07 -0400

    Update sketch.js

--- p5/Using Mouse/sketch.js
+++ p5/Using Mouse/sketch.js
@@ -1,36 +1,21 @@
+var myImage;
+var xPos = 200;
+var yPos = 200;
+var speed = 3;
+
+function preload() {
+	myImage = loadImage("Car.png");
+}
+
 function setup() {
-  var myCanvas = createCanvas(640, 480);
-  myCanvas.parent('Mouse');
+	var myCanvas = createCanvas(640, 480);
+  	myCanvas.parent('Mouse');
 }
 
 function draw() {
-
-}
-
-function mouseMoved() {
-	background(255);
-	// Draw the car body
-	noStroke();
-	fill(35, 235, 250);
-	rect(mouseX, 200, 100, 20);
-	rect(mouseX + 15, 178, 70, 40);
-
-	// draw the wheels
-	fill(77, 66, 66);
-	ellipse(mouseX + 25, 221, 26, 26);
-	ellipse(mouseX + 75, 221, 26, 26);
-}
-
-function mouseClicked() {
-	background(255);
-	// Draw the car body
-	noStroke();
-	fill(35, 235, 250);
-	rect(mouseX, 200, random(100), random(20));
-	rect(mouseX + 15, 178, random(70), random(40));
-
-	// draw the wheels
-	fill(77, 66, 66);
-	ellipse(mouseX + 25, 221, random(26), random(26));
-	ellipse(mouseX + 75, 221, random(26), random(26));
+	image(myImage, xPos, yPos);
+	xPos = xPos + speed;
+	if (xPos >= 640) {
+		xPos = 200;
+	}
 }
