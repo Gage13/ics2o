a9cb14c8c48ffb5ef65460109f05c4b34674052a
Gage13 <dplak1@ocdsb.ca>
2017-05-03 14:03:15 -0400

    Add files via upload

---
+++ p5/Traffic Lights/sketch.js
@@ -0,0 +1,16 @@
+/* Traffic Lights
+Made by: Dragi Plakalovic
+Date: 2017-05-02 */
+
+function setup() {
+	var trafficLights = createCanvas(640, 480);
+	trafficLights.parent('Traffic_Lights');
+}
+
+function draw() {
+	background(39, 245, 70);
+	
+	fill(128, 135, 129);
+	rect(320, 0, 10, 480);
+	// rect();
+}
