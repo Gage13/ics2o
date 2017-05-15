e7b3e247c0f5495fed6eb547c6ed3e6d1fc60c13
Gage13 <dplak1@ocdsb.ca>
2017-05-15 14:08:03 -0400

    Add files via upload

--- p5/KeyBoard/sketch.js
+++ p5/KeyBoard/sketch.js
@@ -5,10 +5,14 @@ Date: 2017-04-28 */
 // This are variables to draw a note slip
 var posX = 100;
 var posY = 100;
-var slipSize = 50;
+var slipSize = 150;
+
+// coordinates for the letters in the array
+var lettersX;
+var lettersY;
 
 // This array will hold letters (initial and typed ones)
-var scriblings = ['W', 'r', 'i', 't', 'e' 'h', 'e', 'r', 'e'];
+var letters = ['W', 'r', 'i', 't', 'e'];
 
 function setup() {
 	var sketch1 = createCanvas(640, 480);
@@ -20,5 +24,32 @@ function draw() {
 	
 	// Create the note slip
 	fill(245,222,179);
-	rect(posX, posY, slipSize, (slipSize*2));
+	rect(posX, posY, slipSize, slipSize);
+	
+	// This for loop will display the letters stored in the array
+	for (var i = 0; i < letters.length; i++) {
+		fill(0);
+		lettersX = (i*15) % (slipSize - 40) + (posX + 20);
+		lettersY = posY + 20 + 15 * floor((i*15) / (slipSize - 40));
+		text(letters[i], lettersX, lettersY);
+	}
+}
+
+function keyTyped() {
+	// Move the note when A is typed
+	if (key === 'd') {
+		posX++;
+	}
+	
+	if (key === 'a') {
+		posX--;
+	}
+	
+	if (key === 'w') {
+		posY--;
+	}
+	
+	if (key === 's') {
+		posY++;
+	}
 }
