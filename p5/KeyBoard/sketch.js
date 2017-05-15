5b903018b78c6b91225e24e6618d62369a3aecd8
Gage13 <dplak1@ocdsb.ca>
2017-05-15 13:24:15 -0400

    Add files via upload

---
+++ p5/KeyBoard/sketch.js
@@ -0,0 +1,24 @@
+/* Using keyboard
+Made by: Gage13
+Date: 2017-04-28 */
+
+// This are variables to draw a note slip
+var xPos = 100;
+var yPos = 100;
+var slipSize = 50;
+
+// This array will hold letters (initial and typed ones)
+var scriblings = ['W', 'r', 'i', 't', 'e' 'h', 'e', 'r', 'e'];
+
+function setup() {
+	var sketch1 = createCanvas(640, 480);
+	sketch1.parent('Keyboard');
+}
+
+function draw() {
+	background(139,69,19);
+	
+	// Create the note slip
+	fill(245,222,179);
+	rect(xPOs, yPos, slipSize, (slipSize*2));
+}
