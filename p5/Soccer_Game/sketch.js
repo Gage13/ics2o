077052374c4f0dd9a43e9efe8c022fd1730e3585
Gage13 <dplak1@ocdsb.ca>
2017-06-07 18:05:52 -0400

    Update sketch.js

--- p5/Soccer_Game/sketch.js
+++ p5/Soccer_Game/sketch.js
@@ -255,20 +255,23 @@ function moveThePlayer() {
 		mySound.stop();
 	}
 	
-	function keyPressed() {return false;}
-	
 	// Control the player
-	if (keyIsDown(UP_ARROW)) {
-		yPos = yPos - p1Vel;
-	}
-	if (keyIsDown(DOWN_ARROW)) {
-		yPos = yPos + p1Vel;
-	}
-	if (keyIsDown(LEFT_ARROW)) {
-		xPos = xPos - p1Vel;
-	}
-	if(keyIsDown(RIGHT_ARROW)) {
-		xPos = xPos + p1Vel;
+	function keyTyped() {
+		if (key === 'w') {
+			yPos = yPos - p1Vel;
+		}
+		if (key === 's') {
+			yPos = yPos + p1Vel;
+		}
+		if (key === 'a') {
+			xPos = xPos - p1Vel;
+		}
+		if (key === 'd') {
+			xPos = xPos + p1Vel;
+		}
+		else {
+			return false;
+		}
 	}
 }
 
