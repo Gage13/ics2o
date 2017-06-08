4f8ac4a854848797f17b797501fa9922df568594
Gage13 <dplak1@ocdsb.ca>
2017-06-08 17:19:59 -0400

    Create sketch.js

--- p5/Soccer_Game/sketch.js
+++ p5/Soccer_Game/sketch.js
@@ -271,7 +271,7 @@ function scoreRecorder() {
 function moveTheHomePlayer() {
 	// Have a player to control the ball
 	playerHome(homeXPos, homeYPos);
-	if ((abs(centerX - homeXPos) <= 20) && (abs(centerY - homeYPos) <= 20)) {
+	if ((abs(centerX - homeXPos) <= 5) && (abs(centerY - homeYPos) <= 5)) {
 		velocityX = -velocityX;
 		velocityY = velocityY;
 		mySound.stop();
